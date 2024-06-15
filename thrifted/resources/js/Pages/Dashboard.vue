a34d4fd14fe0b3601a3f6c1610088e6c746dbf6e
afadel151 <117863082+afadel151@users.noreply.github.com>
2024-06-15 01:36:33 +0100

    primevue integrate

---
+++ thrifted/resources/js/Pages/Dashboard.vue
@@ -0,0 +1,135 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import Button from "primevue/button";
+import SplitButton from 'primevue/splitbutton';
+
+const items = [
+    {
+        label: 'Update',
+        icon: 'pi pi-refresh',
+        command: () => {
+            toast.add({ severity: 'success', summary: 'Updated', detail: 'Data Updated', life: 3000 });
+        }
+    },
+    {
+        label: 'Delete',
+        icon: 'pi pi-times',
+        command: () => {
+            toast.add({ severity: 'warn', summary: 'Delete', detail: 'Data Deleted', life: 3000 });
+        }
+    },
+    {
+        label: 'Vue Website',
+        icon: 'pi pi-external-link',
+        command: () => {
+            window.location.href = 'https://vuejs.org/';
+        }
+    },
+    { label: 'Upload', icon: 'pi pi-upload', to: '/fileupload' }
+];
+
+const save = () => {
+    // toast.add({ severity: 'success', summary: 'Success', detail: 'Data Saved', life: 3000 });
+};
+import CascadeSelect from 'primevue/cascadeselect';
+import { ref } from "vue";
+
+const selectedCity = ref();
+const countries = ref([
+    {
+        name: 'Australia',
+        code: 'AU',
+        states: [
+            {
+                name: 'New South Wales',
+                cities: [
+                    { cname: 'Sydney', code: 'A-SY' },
+                    { cname: 'Newcastle', code: 'A-NE' },
+                    { cname: 'Wollongong', code: 'A-WO' }
+                ]
+            },
+            {
+                name: 'Queensland',
+                cities: [
+                    { cname: 'Brisbane', code: 'A-BR' },
+                    { cname: 'Townsville', code: 'A-TO' }
+                ]
+            }
+        ]
+    },
+    {
+        name: 'Canada',
+        code: 'CA',
+        states: [
+            {
+                name: 'Quebec',
+                cities: [
+                    { cname: 'Montreal', code: 'C-MO' },
+                    { cname: 'Quebec City', code: 'C-QU' }
+                ]
+            },
+            {
+                name: 'Ontario',
+                cities: [
+                    { cname: 'Ottawa', code: 'C-OT' },
+                    { cname: 'Toronto', code: 'C-TO' }
+                ]
+            }
+        ]
+    },
+    {
+        name: 'United States',
+        code: 'US',
+        states: [
+            {
+                name: 'California',
+                cities: [
+                    { cname: 'Los Angeles', code: 'US-LA' },
+                    { cname: 'San Diego', code: 'US-SD' },
+                    { cname: 'San Francisco', code: 'US-SF' }
+                ]
+            },
+            {
+                name: 'Florida',
+                cities: [
+                    { cname: 'Jacksonville', code: 'US-JA' },
+                    { cname: 'Miami', code: 'US-MI' },
+                    { cname: 'Tampa', code: 'US-TA' },
+                    { cname: 'Orlando', code: 'US-OR' }
+                ]
+            },
+            {
+                name: 'Texas',
+                cities: [
+                    { cname: 'Austin', code: 'US-AU' },
+                    { cname: 'Dallas', code: 'US-DA' },
+                    { cname: 'Houston', code: 'US-HO' }
+                ]
+            }
+        ]
+    }
+]);
+</script>
+
+<template>
+    <Head title="Dashboard" />
+    
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-gray-800 text-xl leading-tight">Dashboard</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto sm:px-6 lg:px-8 max-w-7xl">
+                <div class="bg-white shadow-sm p- p-5 sm:rounded-lg overflow-hidden">
+                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <Button label="Submit" />
+                    <SplitButton label="Save" icon="pi pi-plus" @click="save" :model="items" class="bg-slate-600" />
+                    <CascadeSelect v-model="selectedCity" :options="countries" optionLabel="cname" optionGroupLabel="name"
+            :optionGroupChildren="['states', 'cities']" style="min-width: 14rem" placeholder="Select a City" />
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
